0b2126a7c5f7b5dbcb98fa9d3f9d56e46ae05ced
Lorenzo Vega <vega0041@umn.edu>
2023-04-27 22:47:02 -0500

    DroneDecorator.h draft 1 with comments

--- libs/transit/include/DroneDecorator.h
+++ libs/transit/include/DroneDecorator.h
@@ -1,1 +1,36 @@
-//creating branch
+#ifndef DRONE_DECORATOR_H_
+#define DRONE_DECORATOR_H_
+
+#include "IEntity.h"
+
+/**
+ * @brief this class inhertis from the IEntity class and is represents
+ * an abstract drone decorator template.
+ */
+class DroneDecorator : public Drone {
+ protected:
+  IEntity* drone;
+  
+ public:
+  /**
+   * @brief Construct a new Drone Decorator object
+   *
+   * @param drone the drone to decorate onto
+   */
+  DroneDecorator(IEntity* drone);
+  
+  /**
+   * @brief Drone Decorator Destructor
+   */
+  ~DroneDecorator();
+  
+  /**
+   * @brief update function to be overwritten
+   * 
+   * @param dt Delta Time
+   * @param scheduler List of all entities in the system
+   */
+  virtual void Update(double dt, std::vector<IEntity*> scheduler) = 0;
+};
+
+#endif
