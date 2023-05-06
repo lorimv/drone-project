ec7a0111d7bacc35f570d08e4639129521e68885
Joseph <lang1107@umn.edu>
2023-05-05 20:23:57 -0500

    CPPlint

--- libs/transit/include/BatteryDecorator.h
+++ libs/transit/include/BatteryDecorator.h
@@ -16,36 +16,30 @@
     bool charging = false;
     IEntity* simDrone = new Drone();
 
-  
    public:
     /**
      * @brief Construct a new Spin Decorator object
      *
      * @param strategy the strategy to decorate onto
      */
-    BatteryDecorator(IEntity* drone) : DroneDecorator(drone) {};
-    
+    BatteryDecorator(IEntity* drone) : DroneDecorator(drone) {}
     /**
      * @brief BatteryDecorator Destructor
      */
     ~BatteryDecorator();
-    
     /**
      * @brief Update the drone factoring in battery logic
      */
     virtual void Update(double dt, std::vector<IEntity*> scheduler);
-
     /**
      * @brief Getter for accessing battery level
      */
     float GetBatteryLevel() { return charge; }
-
     /**
      * @brief Checks if drone has enough charge to complete the trip
      * and get to the nearest recharge station after the trip.
      */
     bool NeedsCharge(double dt, std::vector<IEntity*> scheduler);
-
     /**
      * @brief Returns the nearest available recharge station
      */
